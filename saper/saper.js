3d149045a8828f767d1c90c30bedd467896036f2
Grasol <34212804+Grasol@users.noreply.github.com>
2022-02-13 15:21:31 +0100

    Add files via upload

--- saper/saper.js
+++ saper/saper.js
@@ -2,30 +2,31 @@ var height = document.getElementById("height_val");
 var width = document.getElementById("width_val");
 var bombs = document.getElementById("bombs_val");
 
-var HIDDEN_TILE = "hidden_tile.svg";
-var EMPTY_TILE = "empty_tile.svg";
-var BOMB_TILE = "bomb_tile.svg";
-var GREEN_BOMB_TILE = "green_bomb_tile.svg";
-var RED_BOMB_TILE = "red_bomb_tile.svg";
-var FLAG_TILE = "flag_tile.svg";
-var QM_TILE = "qm_tile.svg";
-var ONE_TILE = "1_tile.svg";
-var TWO_TILE = "2_tile.svg";
-var THREE_TILE = "3_tile.svg";
-var FOUR_TILE = "4_tile.svg";
-var FIVE_TILE = "5_tile.svg";
-var SIX_TILE = "6_tile.svg";
-var SEVEN_TILE = "7_tile.svg";
-var EIGHT_TILE = "8_tile.svg";
+const HIDDEN_TILE = "hidden_tile.svg";
+const EMPTY_TILE = "empty_tile.svg";
+const BOMB_TILE = "bomb_tile.svg";
+const GREEN_BOMB_TILE = "green_bomb_tile.svg";
+const RED_BOMB_TILE = "red_bomb_tile.svg";
+const FLAG_TILE = "flag_tile.svg";
+const QM_TILE = "qm_tile.svg";
+const ONE_TILE = "1_tile.svg";
+const TWO_TILE = "2_tile.svg";
+const THREE_TILE = "3_tile.svg";
+const FOUR_TILE = "4_tile.svg";
+const FIVE_TILE = "5_tile.svg";
+const SIX_TILE = "6_tile.svg";
+const SEVEN_TILE = "7_tile.svg";
+const EIGHT_TILE = "8_tile.svg";
 
-var LEFT_CLICK = 1;
-var MIDDLE_CLICK = 2;
-var RIGHT_CLICK = 3;
+const LEFT_CLICK = 1;
+const MIDDLE_CLICK = 2;
+const RIGHT_CLICK = 3;
 
-var board;
 var logic;
-var first_click;
-var time;
+var board;
+
+var first_click = true;
+var time = 0;
 var intv;
 
 var Board = class {
@@ -38,7 +39,7 @@ var Board = class {
     this.FLAG = "FLAG";
     this.QM = "QM";
 
-    for (var i = 0; i < this.max_tiles; i += 1) {
+    for (let i = 0; i < this.max_tiles; i++) {
       let array_tile = {
         "bottom_layer": null,
         // EMPTY
@@ -60,7 +61,7 @@ var Board = class {
   }
 
   setTile(idx, bottom=this.EMPTY, top=this.EMPTY) {
-    var array_tile = this.board[idx];
+    let array_tile = this.board[idx];
     if ([this.EMPTY, this.BOMB, 1, 2, 3, 4, 5, 6, 7, 8].includes(bottom)) {
       array_tile["bottom_layer"] = bottom;
     }
@@ -100,22 +101,22 @@ var Logic = class {
 
   fillBoardDigits() {
     // fill board a digits
-    for (var i = 0; i < this.max_tiles; i += 1) {
+    for (let i = 0; i < this.max_tiles; i++) {
       if (this.board.getBottom(i) == this.board.BOMB) {
         continue;
       }
 
-      var tiles = this.getIdxTiles(i);
+      let tiles = this.getIdxTiles(i);
 
-      var num = 0;
-      for (var j = 0; j < 8; j += 1) {
-        var idx_tile = tiles[j];
+      let num = 0;
+      for (let j = 0; j < 8; j++) {
+        let idx_tile = tiles[j];
         if (idx_tile == null) {
           continue;
         } 
 
         if (this.board.getBottom(idx_tile) == this.board.BOMB) {
-          num += 1;
+          num++;
         }
       }
 
@@ -129,11 +130,20 @@ var Logic = class {
     }
   }
 
-  fillBoardBombs(start_idx_tile) {
-    for (var i = 0; i < this.bombs; i += 1) {
+  fillBoardBombs(start_tile_idx) {
+    let tiles = this.getIdxTiles(start_tile_idx);
+
+    for (let i = 0; i < this.bombs; i++) {
+      let empty_try = 3;
       while (true) {
         let r = getRandomInt(0, this.max_tiles);
-        if (r == start_idx_tile) {
+        if (r == start_tile_idx) {
+          continue;
+        }
+
+        else if (tiles.includes(r) && empty_try > 0) {
+          console.log(empty_try, "ZEROZERO");
+          empty_try -= 1;
           continue;
         }
 
@@ -148,12 +158,12 @@ var Logic = class {
   }
 
   visTiles(pos) {
-    var vis = pos;
-    var vis_histry = pos
+    let vis = pos;
+    let vis_histry = pos
     while (true) {
-      var tmp_vis = [];
-      for (var v = 0; v < vis.length; v += 1) {
-        var vis_tile = this.board.getBottom(vis[v]);
+      let tmp_vis = [];
+      for (let v = 0; v < vis.length; v++) {
+        let vis_tile = this.board.getBottom(vis[v]);
         if (vis_tile != this.board.EMPTY) {
           if (([1, 2, 3, 4, 5, 6, 7, 8].includes(vis_tile)) ||
               (vis_tile == this.board.BOMB)) {
@@ -164,8 +174,8 @@ var Logic = class {
         }
 
         this.board.setTile(vis[v], null, false);
-        var tiles = this.getIdxTiles(vis[v]);
-        for (var i = 0; i < 8; i += 1) {
+        let tiles = this.getIdxTiles(vis[v]);
+        for (let i = 0; i < 8; i++) {
           if (tiles[i] == null) {
             continue;
           }
@@ -188,8 +198,8 @@ var Logic = class {
   }
 
   getIdxTiles(pos) {
-    var res = new Array(8);
-    var border;
+    let res = new Array(8);
+    let border;
 
     border = this.hBorderTiles(pos);
     if (pos > border[0]) {
@@ -200,7 +210,7 @@ var Logic = class {
       res[1] = pos + 1;
     }
 
-    var pos2 = pos - this.width;
+    let pos2 = pos - this.width;
     border = this.hBorderTiles(pos2);
     if (pos2 >= 0) {
       res[3] = pos2;
@@ -232,24 +242,24 @@ var Logic = class {
   }
 
   hBorderTiles(pos) {
-    var min = Math.floor(pos / this.width) * this.width;
-    var max = min + this.width - 1;
+    let min = Math.floor(pos / this.width) * this.width;
+    let max = min + this.width - 1;
 
     return [min, max];
   }
 
   tileService(e) {
-  var res = null;
+  let res = null;
 
-  var num_tile = parseInt(e.target.id.substr(5));
+  let num_tile = parseInt(e.target.id.substr(5));
   if (first_click) {
     first_click = false;
     this.fillBoardBombs(num_tile);
     this.fillBoardDigits();
   }
 
-  var tile_bottom = this.board.getBottom(num_tile);
-  var tile_top = this.board.getTop(num_tile);
+  let tile_bottom = this.board.getBottom(num_tile);
+  let tile_top = this.board.getTop(num_tile);
 
   // service click 
   switch(e.which) {
@@ -264,18 +274,18 @@ var Logic = class {
   case MIDDLE_CLICK: {
     if (tile_top == false) {
       if ([1, 2, 3, 4, 5, 6, 7, 8].includes(tile_bottom)) {
-        var tiles = this.getIdxTiles(num_tile);
-        var vis = [];
-        var flags = 0
-        for (var i = 0; i < 8; i += 1) {
+        let tiles = this.getIdxTiles(num_tile);
+        let vis = [];
+        let flags = 0
+        for (let i = 0; i < 8; i++) {
           if (tiles[i] == null) {
             continue;
           }
 
-          var t = this.board.getTop(tiles[i]);
+          let t = this.board.getTop(tiles[i]);
           console.log(tiles[i], t);
           if (t == this.board.FLAG) {
-            flags += 1;
+            flags++;
           }
 
           else if (t == this.board.EMPTY) {
@@ -293,9 +303,9 @@ var Logic = class {
 
   case RIGHT_CLICK: {
     switch (tile_top) {
-      case this.board.EMPTY: this.board.setTile(num_tile, null, board.FLAG);  break;
-      case this.board.FLAG:  this.board.setTile(num_tile, null, board.QM);    break;
-      case this.board.QM:    this.board.setTile(num_tile, null, board.EMPTY); break;
+      case this.board.EMPTY: this.board.setTile(num_tile, null, this.board.FLAG);  break;
+      case this.board.FLAG:  this.board.setTile(num_tile, null, this.board.QM);    break;
+      case this.board.QM:    this.board.setTile(num_tile, null, this.board.EMPTY); break;
     }
 
     break;
@@ -303,14 +313,14 @@ var Logic = class {
   }
   
   // check status game
-  var status = 0;
+  let status = 0;
   // 0: ok
   // 1: win
   // 2: boom
  
   console.log(">>>>>", res);
   if (res != null) {
-    for (var i = 0; i < res.length; i += 1) {
+    for (let i = 0; i < res.length; i++) {
       if (this.board.getBottom(res[i]) == this.board.BOMB) {
         console.log("AA")
         status = 2;
@@ -321,8 +331,8 @@ var Logic = class {
 
   if (status == 0) {
     cont: {
-    for (var i = 0; i < this.board.board.length; i += 1) {
-      var t = this.board.getTop(i);
+    for (let i = 0; i < this.board.board.length; i++) {
+      let t = this.board.getTop(i);
       console.log("<<<", t);
       if (t == this.board.EMPTY || t == this.board.QM) {
         break cont;
@@ -361,36 +371,36 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function updateSrcBoard(status) {
-  var bomb_variant = 0;
-  var flag_variant = 0;
+function updateSrcBoard(logic, status) {
+  let bomb_variant = 0;
+  let flag_variant = 0;
   switch (status) {
     case 1: flag_variant = 1; break;
     case 2: bomb_variant = 1; break; 
   }
 
-  for (var i = 0; i < logic.max_tiles; i += 1) {
-    var tile_id = "tile-" + String(i);
-    var tile = document.getElementById(tile_id);
-    tile.src = "assets/" + getTileLink(i, bomb_variant, flag_variant);
+  for (let i = 0; i < logic.max_tiles; i++) {
+    let tile_id = "tile-" + String(i);
+    let tile = document.getElementById(tile_id);
+    tile.src = "assets/" + getTileLink(logic.board, i, bomb_variant, flag_variant);
   }
 }
 
-function genBoard() {
-  var h_max = logic.height;
-  var w_max = logic.width;
-  var saperBoard = document.getElementById("saper");
+function genBoard(logic) {
+  let h_max = logic.height;
+  let w_max = logic.width;
+  let saperBoard = document.getElementById("saper");
 
-  h_tiles = new Array();
-  for (var h = 0; h < h_max; h += 1) {
+  let h_tiles = new Array();
+  for (let h = 0; h < h_max; h++) {
     w_tiles = new Array();
-    for (var w = 0; w < w_max; w += 1) {
-      var tile_idx = h * w_max + w;
+    for (let w = 0; w < w_max; w++) {
+      let tile_idx = h * w_max + w;
 
-      var tile_link = "assets/" + HIDDEN_TILE;
-      var tile_id = "tile-" + String(tile_idx);
+      let tile_link = "assets/" + HIDDEN_TILE;
+      let tile_id = "tile-" + String(tile_idx);
 
-      var img_text = ("<img id=" + tile_id + 
+      let img_text = ("<img id=" + tile_id + 
                       " src='" + tile_link + 
                       "' height='25' width='25'>")
 
@@ -404,7 +414,7 @@ function genBoard() {
 
 }
 
-function getTileLink(num_tile, bomb_variant=0, flag_variant=0) {
+function getTileLink(board, num_tile, bomb_variant=0, flag_variant=0) {
   // bomb_variant
   // 0: normal_bomb
   // 1: red_bomb
@@ -413,10 +423,10 @@ function getTileLink(num_tile, bomb_variant=0, flag_variant=0) {
   // 0: flag 
   // 1: green_bomb 
 
-  var tile_bottom = board.getBottom(num_tile);
-  var tile_top = board.getTop(num_tile);  
+  let tile_bottom = board.getBottom(num_tile);
+  let tile_top = board.getTop(num_tile);  
 
-  var res;
+  let res;
   if (tile_top == false) {
     switch (tile_bottom) {
       case board.EMPTY: res = EMPTY_TILE; break;
@@ -456,7 +466,7 @@ function getTileLink(num_tile, bomb_variant=0, flag_variant=0) {
 }
 
 function counter() {
-  time += 1;
+  time++;
   document.getElementById("time").innerHTML = time;
 }
 
@@ -465,15 +475,16 @@ function counter() {
 function main_game() {
   first_click = true;
   time = 0;
- 
-  logic = new Logic(null)
-  board = new Board(logic.max_tiles);
+
+  var logic = new Logic(null)
+  console.log(logic.height, "AAAAAAA");
+  var board = new Board(logic.max_tiles);
   logic.board = board;
 
-  genBoard(board)
+  genBoard(logic);
 
-  var print_tiles = new Array(board.max_tiles);
-  for (var i = 0; i < board.max_tiles; i += 1) {
+  let print_tiles = new Array(board.max_tiles);
+  for (let i = 0; i < board.max_tiles; i++) {
     print_tiles[i] = document.getElementById("tile-" + String(i));
   }
 
@@ -487,21 +498,21 @@ function main_game() {
         intv = window.setInterval(counter, 1000);
       }
 
-      var status = logic.tileService(e);
+      let status = logic.tileService(e);
       
-      updateSrcBoard(status);
-      var flags = 0
-      for (var i = 0; i < board.board.length; i += 1) {
+      updateSrcBoard(logic, status);
+      let flags = 0
+      for (let i = 0; i < board.board.length; i++) {
         if (board.getTop(i) == board.FLAG) {
-          flags += 1;
+          flags++;
         }
       }
 
       document.getElementById("bombs_num").innerHTML = logic.bombs - flags;
       if (status != 0) {
         window.clearInterval(intv);
-        var saper = document.getElementById("saper")
-        var clone_saper = saper.cloneNode(true);
+        let saper = document.getElementById("saper")
+        let clone_saper = saper.cloneNode(true);
         document.body.replaceChild(clone_saper, saper);
       }
     })
